384d95650ee3a5c0f2931cb3c463471bf8a2220a
Kris Kristensen <KRKR1303@dsb.dk>
2021-11-11 13:20:22 +0100

    viewbinding

--- app/src/main/java/com/smartai/fragments/SearchFragment.kt
+++ app/src/main/java/com/smartai/fragments/SearchFragment.kt
@@ -5,9 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.smartai.model.Connector
+import com.smartai.model.Request
 import com.smartai.smartAI.R
+import com.smartai.smartAI.databinding.FragmentSearchBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
+
+    private val ui by userInterface(FragmentSearchBinding::bind)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_search, container, false)
@@ -15,6 +23,20 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        ui.searchSubmit.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val connector = Connector("kundecenter", "kundecenter")
+                val request = Request.newInstance()
+
+                request.server = "http://salgskerne-tst1.dsb.dk/sales/version"
+
+                connector.get(request)
+
+                println(request.response)
+            }
+        }
+
     }
 
 }
